3ceb7d7c749919a2dffda475b7349d1044756073
shafi_007 <shafiptb07@gmail.com>
2022-11-07 13:21:53 +0530

    #port changed

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const routes  = require("./routes");
 
 const app = express()
-const PORT = 8000
+const PORT = process.env.PORT || 8000
 
 // commonQueryModel.exexuteQuery()
 app.use(cors())
