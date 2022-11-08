b86a0ce549159190a5f4c38699db74a299aa5969
shafi_007 <shafiptb07@gmail.com>
2022-11-08 16:52:02 +0530

    #db string changed

--- src/helpers/dbConnection.js
+++ src/helpers/dbConnection.js
@@ -1,7 +1,9 @@
 const { Pool } = require('pg')
+const NEW_STRING = `postgres://ubuntu:password@ec2-3-110-216-253.ap-south-1.compute.amazonaws.com:5432/alinma-dev`
 
 const client = new Pool({
-    connectionString: 'postgres://shafi_07:shafi123@alinma.ccj1mloxjvmp.ap-south-1.rds.amazonaws.com:5432/alinama_test',
+    // connectionString: 'postgres://shafi_07:shafi123@alinma.ccj1mloxjvmp.ap-south-1.rds.amazonaws.com:5432/alinama_test',
+    connectionString:NEW_STRING,
     connectionTimeoutMillis:100000
 })
 
