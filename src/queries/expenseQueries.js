accd5e5a94f0613bc2275a2ef6eeb4df18d0d530
shafi_007 <shafiptb07@gmail.com>
2022-11-18 19:16:34 +0530

    #query fix

--- src/queries/expenseQueries.js
+++ src/queries/expenseQueries.js
@@ -1,11 +1,11 @@
-const CREATE_NEW_EXPENSE = `INSERT INTO visa (electricity,telephone,salary,stationary,other,createdUser, updatedUser,remarks) 
+const CREATE_NEW_EXPENSE = `INSERT INTO expense (electricity,telephone,salary,stationary,other,createdUser, updatedUser,remarks) 
     VALUES ($1,$2,$3,$4,$5,$6,$7,$8)`
 
 const GET_ALL_EXPENSE = `SELECT DISTINCT ON(createdTime) id,total_amount,fileId,electricity,telephone,salary,stationary,other,remarks,createdTime 
-    FROM visa ORDER BY createdTime DESC LIMIT 100`
+    FROM expense ORDER BY createdTime DESC LIMIT 100`
 
 const GET_ALL_EXPENSE_QUERY = `SELECT DISTINCT ON(createdTime) id,total_amount,fileId,electricity,telephone,salary,stationary,other,remarks,createdTime
-    FROM visa WHERE createdTime ILIKE '%' || $1 || '%'  LIMIT 100`
+    FROM expense WHERE createdTime ILIKE '%' || $1 || '%'  LIMIT 100`
 
 // const GET_ALL_VISA_STATUS = `SELECT DISTINCT ON(fileId,name) id,name,id_number,sponser_name,visa_number,
 //     total_amount,paid_amount,mobileNumber,sub_category,balance_amount,fileId,status,agent,agent_amount,paid_date,service,remarks
@@ -15,10 +15,10 @@ const GET_ALL_EXPENSE_QUERY = `SELECT DISTINCT ON(createdTime) id,total_amount,f
 //     total_amount,paid_amount,mobileNumber,sub_category,balance_amount,fileId,status,agent,agent_amount,paid_date,service,remarks
 //     FROM visa WHERE (fileId ILIKE '%' || $1 || '%' OR name ILIKE '%' || $1 || '%') AND status = $2  LIMIT 100`
 
-const UPDATE_EXPENSE = `UPDATE visa SET electricity=$2,telephone=$3,salary=$4,stationary=$5,other=$6,createdUser=$7, remarks=$8 WHERE id = $1`
+const UPDATE_EXPENSE = `UPDATE expense SET electricity=$2,telephone=$3,salary=$4,stationary=$5,other=$6,createdUser=$7, remarks=$8 WHERE id = $1`
 
 
-const DELETE_EXPENSE = `DELETE FROM visa WHERE id = $1`
+const DELETE_EXPENSE = `DELETE FROM expense WHERE id = $1`
 
 module.exports = {
     CREATE_NEW_EXPENSE,
