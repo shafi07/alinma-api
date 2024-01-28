47c05b594e8b83516c0729f616b336d2e3c2596a
shafi_007 <shafiptb07@gmail.com>
2024-01-29 03:56:08 +0530

    #boarder number added

--- src/controllers/javasthController.js
+++ src/controllers/javasthController.js
@@ -90,9 +90,9 @@ module.exports = {
     async patchJavasath(req, res) {
         try {
             const { id, agent_amount, agent, paid_date, sponser_name, name, id_number, purpose, iqama, insurance,absheer_amount,qiwa_amount,government_fee,new_passport_number,expiry_date,
-                other, total_amount, paid_amount, service, mobileNumber, mol, sub_category, remarks, professionName, newSponser, due,re_entry_type } = req.body
+                other, total_amount, paid_amount, service, mobileNumber, mol, sub_category, remarks, professionName, newSponser, due,re_entry_type,boarder_number } = req.body
             commonQuery.exexuteQuery(javsath.PATCH_JAVASATH, [id, agent, agent_amount, paid_date, sponser_name, name, id_number, purpose, iqama, insurance,
-                other, total_amount, paid_amount, service, mobileNumber, mol, sub_category, remarks, professionName, newSponser, due,absheer_amount,qiwa_amount,government_fee,new_passport_number,expiry_date,re_entry_type])
+                other, total_amount, paid_amount, service, mobileNumber, mol, sub_category, remarks, professionName, newSponser, due,absheer_amount,qiwa_amount,government_fee,new_passport_number,expiry_date,re_entry_type,boarder_number])
             return res.status(200).json({
                 message: "Javasath Data Updated successfully",
             });
@@ -106,11 +106,11 @@ module.exports = {
 async function newJavasath(data) { 
     try {
         const { sponser_name, name, id_number, purpose, iqama, insurance, other, total_amount,absheer_amount,qiwa_amount,government_fee,new_passport_number,expiry_date,
-            paid_amount, service, mobileNumber, createdUser, updatedUser, mol, sub_category, remarks, agent, agent_amount, paid_date, professionName, newSponser,due,re_entry_type } = data
+            paid_amount, service, mobileNumber, createdUser, updatedUser, mol, sub_category, remarks, agent, agent_amount, paid_date, professionName, newSponser,due,re_entry_type,boarder_number } = data
         let date = moment().format("DD-MM-YYYY")
         let javasath = await commonQuery.exexuteQuery(javsath.CREATE_NEW_JAVASATH, [sponser_name, name, id_number, purpose, iqama, insurance, other, total_amount,
             paid_amount, service, mobileNumber, createdUser, updatedUser, mol, sub_category, remarks, (`{"amount":"${paid_amount}","date":"${date}"}`), agent, agent_amount, paid_date, professionName, newSponser, due,
-            absheer_amount,qiwa_amount,government_fee,new_passport_number,expiry_date,re_entry_type ])
+            absheer_amount,qiwa_amount,government_fee,new_passport_number,expiry_date,re_entry_type,boarder_number ])
         return true
     } catch (error) {
         console.log(error)
--- src/queries/javasthQueries.js
+++ src/queries/javasthQueries.js
@@ -1,28 +1,28 @@
 const CREATE_NEW_JAVASATH = `INSERT INTO javasath (sponser_name,name,id_number,purpose,iqama,insurance,other,
-total_amount,paid_amount,service,mobileNumber,createdUser,updatedUser,mol,sub_category,remarks,amount_paid_dates,agent,agent_amount,paid_date,professionName,newSponser,due,absheer_amount,qiwa_amount,government_fee,new_passport_number,expiry_date,re_entry_type) 
-VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,ARRAY[$17]::json[],$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29)`
+total_amount,paid_amount,service,mobileNumber,createdUser,updatedUser,mol,sub_category,remarks,amount_paid_dates,agent,agent_amount,paid_date,professionName,newSponser,due,absheer_amount,qiwa_amount,government_fee,new_passport_number,expiry_date,re_entry_type,boarder_number) 
+VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,ARRAY[$17]::json[],$18,$19,$20,$21,$22,$23,$24,$25,$26,$27,$28,$29,$30)`
 
-const GET_ALL_JAVASATH = `SELECT DISTINCT ON(createdTime) id,sponser_name,name,id_number,purpose,iqama,insurance,other,amount_paid_dates,remarks,agent,agent_amount,paid_date,professionName,newSponser,re_entry_type,
+const GET_ALL_JAVASATH = `SELECT DISTINCT ON(createdTime) id,sponser_name,name,id_number,purpose,iqama,insurance,other,amount_paid_dates,remarks,agent,agent_amount,paid_date,professionName,newSponser,re_entry_type,boarder_number,
 total_amount,paid_amount,service,mobileNumber,mol,sub_category,balance_amount,to_char(createdTime,'DD/MM/YYYY') createdDate,fileId,status,due,absheer_amount,qiwa_amount,government_fee,new_passport_number,expiry_date
 FROM javasath ORDER BY createdTime DESC LIMIT 50`
 
-const GET_ALL_JAVASATH_QUERY = `SELECT id,sponser_name,name,id_number,purpose,iqama,insurance,other,agent,agent_amount,paid_date,professionName,newSponser,re_entry_type,
+const GET_ALL_JAVASATH_QUERY = `SELECT id,sponser_name,name,id_number,purpose,iqama,insurance,other,agent,agent_amount,paid_date,professionName,newSponser,re_entry_type,boarder_number,
 total_amount,paid_amount,service,mobileNumber,mol,sub_category,balance_amount,to_char(createdTime,'DD/MM/YYYY') createdTime,fileId,status,amount_paid_dates,remarks,due,absheer_amount,qiwa_amount,government_fee,new_passport_number,expiry_date
 FROM javasath WHERE name ILIKE '%' || $1 || '%' OR mobileNumber ILIKE '%' || $1 || '%' OR id_number ILIKE '%' || $1 || '%' LIMIT 100`
 
-const GET_ALL_JAVASATH_STATUS = `SELECT id,sponser_name,name,id_number,purpose,iqama,insurance,other,agent,agent_amount,paid_date,professionName,newSponser,re_entry_type,
+const GET_ALL_JAVASATH_STATUS = `SELECT id,sponser_name,name,id_number,purpose,iqama,insurance,other,agent,agent_amount,paid_date,professionName,newSponser,re_entry_type,boarder_number,
 total_amount,paid_amount,service,mobileNumber,mol,sub_category,balance_amount,to_char(createdTime,'DD/MM/YYYY') createdTime,fileId,status,amount_paid_dates,remarks,due,absheer_amount,qiwa_amount,government_fee,new_passport_number,expiry_date
 FROM javasath WHERE status=$1`
 
-const GET_ALL_JAVASATH_STATUS_QUERY = `SELECT id,sponser_name,name,id_number,purpose,iqama,insurance,other,agent,agent_amount,paid_date,professionName,newSponser,re_entry_type,
+const GET_ALL_JAVASATH_STATUS_QUERY = `SELECT id,sponser_name,name,id_number,purpose,iqama,insurance,other,agent,agent_amount,paid_date,professionName,newSponser,re_entry_type,boarder_number,
 total_amount,paid_amount,service,mobileNumber,mol,sub_category,balance_amount,to_char(createdTime,'DD/MM/YYYY') createdTime,fileId,status,amount_paid_dates,remarks,due,absheer_amount,qiwa_amount,government_fee,new_passport_number,expiry_date
 FROM javasath WHERE (name ILIKE '%' || $1 || '%' OR mobileNumber ILIKE '%' || $1 || '%' OR id_number ILIKE '%' || $1 || '%') AND status = $2  LIMIT 100`
 
-const GET_ALL_JAVASATH_CREDIT = `SELECT id,sponser_name,name,id_number,purpose,iqama,insurance,other,agent,agent_amount,paid_date,professionName,newSponser,re_entry_type,
+const GET_ALL_JAVASATH_CREDIT = `SELECT id,sponser_name,name,id_number,purpose,iqama,insurance,other,agent,agent_amount,paid_date,professionName,newSponser,re_entry_type,boarder_number,
 total_amount,paid_amount,service,mobileNumber,mol,sub_category,balance_amount,to_char(createdTime,'DD/MM/YYYY') createdTime,fileId,status,amount_paid_dates,remarks,due,absheer_amount,qiwa_amount,government_fee,new_passport_number,expiry_date
 FROM javasath WHERE balance_amount != '0'`
 
-const GET_ALL_JAVASATH_QUERY_CREDIT = `SELECT id,sponser_name,name,id_number,purpose,iqama,insurance,other,agent,agent_amount,paid_date,professionName,newSponser,re_entry_type,
+const GET_ALL_JAVASATH_QUERY_CREDIT = `SELECT id,sponser_name,name,id_number,purpose,iqama,insurance,other,agent,agent_amount,paid_date,professionName,newSponser,re_entry_type,boarder_number,
 total_amount,paid_amount,service,mobileNumber,mol,sub_category,balance_amount,to_char(createdTime,'DD/MM/YYYY') createdTime,fileId,status,amount_paid_dates,remarks,due,absheer_amount,qiwa_amount,government_fee,new_passport_number,expiry_date
 FROM javasath WHERE (name ILIKE '%' || $1 || '%' OR mobileNumber ILIKE '%' || $1 || '%' OR id_number ILIKE '%' || $1 || '%') AND balance_amount != '0'  LIMIT 100`
 
